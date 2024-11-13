3803de50fd7933f74e67174d85fa4f376645320d
juliakoma <juk97@pitt.edu>
2024-11-13 18:22:15 -0500

    Cleaned up some dead code

--- readInput/test.ino
+++ readInput/test.ino
@@ -43,20 +43,13 @@ int microphoneState = 0;
 int state = 0;
 float volts;
 
-//unsigned long timeLimit = 2000; // Initial time limit for each action (milliseconds)
-//unsigned long actionStartTime;  // Time when action started
-//int currentAction = 0;       // 0 = button press, 1 = twist potentiometer
-//int score = 0;               // Player's score
-//bool gameActive = false;     // Whether the game is ongoing
-bool gameStarted = false;    // Whether the game has started
-
 // Accelerometer Values
 const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
 int16_t accelerometer_x, accelerometer_y; // variables for accelerometer raw data
 int16_t previous_accelerometer_x, previous_accelerometer_y = 0;
 String output;
 char tmp_str[7]; // temporary variable used in convert function
-char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. resulting strings will have the same length in the debug monitor.
   sprintf(tmp_str, "%6d", i);
   return tmp_str;
 }
@@ -64,14 +57,14 @@ char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, r
 void setup() {
   Serial.begin(9600);
 
-  lcd.begin(16,2);//Defining 16 columns and 2 rows of lcd display
-  lcd.backlight();//To Power ON the back light
+  lcd.begin(16,2);
+  lcd.backlight();
 
   //Accelerometer Setup
   Wire.begin();
-  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
-  Wire.write(0x6B); // PWR_MGMT_1 register
-  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.beginTransmission(MPU_ADDR); 
+  Wire.write(0x6B);
+  Wire.write(0);
   Wire.endTransmission(true);
 
   //Initialize pins
@@ -101,13 +94,13 @@ void loop() {
 
   lcd.backlight();
   Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Regis3
-  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
-  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  Wire.write(0x3B); 
+  Wire.endTransmission(false); 
+  Wire.requestFrom(MPU_ADDR, 7*2, true); 
+  accelerometer_x = Wire.read()<<8 | Wire.read(); 
   accelerometer_y = Wire.read()<<8 | Wire.read(); 
 
-  lcd.setCursor(0,0); //Defining positon to write from first row,first column .
+  lcd.setCursor(0,0); 
 
   if (microphoneState == HIGH){
     lcd.clear();
@@ -188,12 +181,6 @@ void execute_CMD(byte CMD, byte Par1, byte Par2)
   }
 }
 
-void setup(){
-
-Serial.begin(9600);
-
-
-}
 
 //MICROPHONE LOGIC THAT NEEDS TO BE ADDED
 // void loop() {
